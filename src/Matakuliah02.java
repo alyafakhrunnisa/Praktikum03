6d6c583053bdd544a9d27d7a024e451c4163f523
alyafakhrunnisa <nisafakhrunalya@gmail.com>
2026-02-25 08:25:07 +0700

    3.4 Mahasiswa

---
+++ src/Matakuliah02.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class Matakuliah02 {
+    public String kode;
+    public String nama;
+    public int sks;
+    public int jumlahJam;
+
+    public Matakuliah02(){
+
+    }
+
+    public Matakuliah02(String kode, String nama, int sks, int jumlahJam) {
+        this.kode = kode;
+        this.nama = nama;
+        this.sks = sks;
+        this.jumlahJam = jumlahJam;    
+    }
+
+    public void tambahData() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Kode       : ");
+        kode = sc.nextLine();
+        System.out.print("Nama       : ");
+        nama = sc.nextLine();
+        System.out.print("SKS        : ");
+        sks = Integer.parseInt(sc.nextLine());
+        System.out.print("Jumlah Jam : ");
+        jumlahJam = Integer.parseInt(sc.nextLine());
+    }
+
+    public void cetakInfo() {
+        System.out.println("Kode       : " + kode);
+        System.out.println("Nama       : " + nama);
+        System.out.println("SKS        : " + sks);
+        System.out.println("Jumlah Jam : " + jumlahJam);
+        System.out.println("-----------------------------------------");
+    }
+}
